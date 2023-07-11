8ebe79105c2c9814bf150605c7fc3d7bfaa71d87
vienze <kevinzkurrniawan@gmail.com>
2023-07-12 00:10:12 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// NAVBAR CHANGE ICON TOGGLER MENU
+const iconTogglerMenu = document.querySelector(".navbar-toggler .bi");
+iconTogglerMenu.addEventListener("click", () => {
+  iconTogglerMenu.classList.toggle("bi-x");
+});
